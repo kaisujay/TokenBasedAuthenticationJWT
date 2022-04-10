89aedfb7042005145f3bb7b7507a15c76abe5927
sujay <sujay@DESKTOP-FHS1GO0>
2022-04-10 16:40:24 +0530

    Add project files.

---
+++ JWTWebApi/Controllers/TokenController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace JWTWebApi.Controllers
+{
+    public class TokenController : ApiController
+    {
+        [HttpGet]
+        //Call like > http://localhost:4074/api/Token?UserName=admin&Password=123
+        public IHttpActionResult ValidateLogin(string userName, string password)
+        {
+            if (userName == "admin" && password == "123")
+            {
+                return Ok(TokenManager.GenerateToken(userName));
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+    }
+}
